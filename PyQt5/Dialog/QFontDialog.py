ad1018bc83e82bc28a7e8c931f80c1a7d4deb237
seogihyun <seogihyun@gmail.com>
2021-03-10 09:30:10 +0900

    practice FontDialog"
    :wq
    exit
    "

---
+++ PyQt5/Dialog/QFontDialog.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QPushButton, QSizePolicy, QLabel, QFontDialog)
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        btn = QPushButton('Dialog', self)
+        btn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        btn.move(20,20)
+        btn.clicked.connect(self.showDialog)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(btn)
+
+        self.lbl = QLabel('The quick brown fox jumps over the lazy dog', self)
+        self.lbl.move(130,20)
+
+        vbox.addWidget(self.lbl)
+        self.setLayout(vbox)
+
+        self.setWindowTitle('Font Dialog')
+        self.setGeometry(300, 300, 250, 180)
+        self.show()
+
+    def showDialog(self):
+        font, ok = QFontDialog.getFont() # 폰트 변경
+
+        if ok:
+            self.lbl.setFont(font) # 선택한 폰트를 라벨의 폰트로 설정
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
